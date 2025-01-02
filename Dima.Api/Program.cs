aa11aad9826c1adbd6b6ba0ab2e4c4f4b1dc9f98
devRafaelColares <rafaelcolares.dev@gmail.com>
2025-01-02 17:02:00 -0300

    Padronizando requisições e respostas para a API.

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,4 +1,5 @@
 using Dima.Api.Data;
+using Dima.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,10 +28,10 @@ app.UseSwaggerUI();
 app.MapGet("/", () => "Hello World!");
 
 app.MapPost
-        ("/v1/transactions",
+        ("/v1/categories",
         (Request request, Handler handler) => handler.Handle(request))
-        .WithName("CreateTransaction")
-        .WithSummary("Create a new transaction")
+        .WithName("CreateCategories")
+        .WithSummary("Create a new category")
         .Produces<Response>();
 
 
@@ -41,11 +42,7 @@ app.Run();
 public class Request
 {
     public string Title { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public int Type { get; set; }
-    public decimal Amount { get; set; }
-    public long CategoryId { get; set; }
-    public string UserId { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 }
 
 public class Response
@@ -55,14 +52,23 @@ public class Response
 
 }
 
-public class Handler
+public class Handler(AppDbContext context)
 {
     public Response Handle(Request request)
     {
+        var category = new Category
+        {
+            Title = request.Title,
+            Description = request.Description,
+        };
+
+        context.Categories.Add(category);
+        context.SaveChanges();
+
         return new Response
         {
-            Id = 4,
-            Title = request.Title,
+            Id = category.Id,
+            Title = category.Title,
         };
     }
 }
