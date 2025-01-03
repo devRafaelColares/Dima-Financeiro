9eedd1bf811d2ae94a58e434c13d6e9791878e78
devRafaelColares <rafaelcolares.dev@gmail.com>
2025-01-03 11:52:00 -0300

    Iniciando padronização e criação de grupos de endpoints.

---
+++ Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,20 @@
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class CreateCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapPost("/", HandleAsync);
+
+        private static async Task<IResult> HandleAsync(ICategoryHandler handler, CreateCategoryRequest request)
+        {
+            var result = await handler.CreateCategoryAsync(request);
+            return result.IsSuccess ? TypedResults.Created($"/{result.Data?.Id}", result.Data) : TypedResults.BadRequest(result.Data);
+        }
+    }
+}
